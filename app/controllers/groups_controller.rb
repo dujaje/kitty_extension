413a1cc5a438d24c02aa645b9815ec38ddd9705b
Jamie Dujardin <jamiedujardin@gmail.com>
2017-12-28 00:36:33 +0200

    can't add kitty without a name

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,8 +18,12 @@ class GroupsController < ApplicationController
     @user = User.find(params[:group][:user_id])
     @group = Group.find(params[:group][:group_id])
     @group.name = params[:group][:name]
-    @group.save
-    redirect_to group_path(@group, user_id: @user.id, group_id: @group.id)
+    if @group.name == ""
+      render 'pages/create_kitty'
+    else
+      @group.save
+      redirect_to group_path(@group, user_id: @user.id, group_id: @group.id)
+    end
   end
 
   def reminder
